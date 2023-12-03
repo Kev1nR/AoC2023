f4f504d7bae2431ead053e7b472411d4c90e6db7
Kev1nR <kevinroche@blueyonder.co.uk>
2023-12-03 15:57:19 +0000

    Day 1 parts 1 and 2 complete

--- Day 1/process.fsx
+++ Day 1/process.fsx
@@ -3,26 +3,71 @@
 
 let filePath = @"Day 1\input-data.txt"
 
-let extractFirstAndLastNumbers cs =
+open System
+
+type Dirn = Forward | Backward
+
+type String with
+    member __.Reverse() =
+        __ |> Seq.rev |> Seq.fold (fun acc c -> acc + (string c)) ""
+
+let numberMatch dirn digit (numberRep: string) (input:string) =
+    let fwdRep, bwdRep = numberRep, (numberRep.Reverse())
+
+    input[0] = digit 
+    || (dirn = Forward && input.StartsWith(fwdRep))
+    || (dirn = Backward && input.StartsWith(bwdRep))
+
+let (|One|_|) dirn (input: string) =    if numberMatch dirn '1' "one"   input then Some 1 else None
+let (|Two|_|) dirn (input: string) =    if numberMatch dirn '2' "two"   input then Some 2 else None
+let (|Three|_|) dirn (input: string) =  if numberMatch dirn '3' "three" input then Some 3 else None
+let (|Four|_|) dirn (input: string) =   if numberMatch dirn '4' "four"  input then Some 4 else None
+let (|Five|_|) dirn (input: string) =   if numberMatch dirn '5' "five"  input then Some 5 else None
+let (|Six|_|) dirn (input: string) =    if numberMatch dirn '6' "six"   input then Some 6 else None
+let (|Seven|_|) dirn (input: string) =  if numberMatch dirn '7' "seven" input then Some 7 else None
+let (|Eight|_|) dirn (input: string) =  if numberMatch dirn '8' "eight" input then Some 8 else None
+let (|Nine|_|) dirn (input: string) =   if numberMatch dirn '9' "nine"  input then Some 9 else None
+
+let rec findNumber dirn (line: string) =
+    match line with
+    | line when line.Length = 0 -> failwith "No number like string found"
+    | One dirn n -> n
+    | Two dirn n -> n
+    | Three dirn n -> n
+    | Four dirn n -> n
+    | Five dirn n -> n
+    | Six dirn n -> n
+    | Seven dirn n -> n
+    | Eight dirn n -> n
+    | Nine dirn n -> n
+    | _ -> findNumber dirn line[1..]
+
+let part1Result (line: string) =
     let tensVal = 
-        cs 
+        line.ToCharArray() 
         |> Array.find (fun c -> fst (System.Int32.TryParse (string c)))
         |> fun c -> let _, parsed = System.Int32.TryParse (string c)
                     parsed
 
     let unitsVal =
-        cs 
+        line.ToCharArray() 
         |> Array.findBack (fun c -> fst (System.Int32.TryParse (string c)))    
         |> fun c -> let _, parsed = System.Int32.TryParse (string c)
                     parsed
 
     (tensVal * 10) + unitsVal
 
-let ss = ReadData.readLines filePath
-         |> Seq.toArray
-         |> Seq.map (fun line -> line.ToCharArray())
-         |> Seq.map (extractFirstAndLastNumbers)
-         |> Seq.sum
+let part2Result (line : string) =
+    let tensVal = line |> findNumber Forward
+    let unitsVal = line.Reverse() |> findNumber Backward
 
+    (tensVal * 10) + unitsVal
 
-printfn "%A" ss
+let run extractor = 
+        ReadData.readLines filePath
+        |> Seq.toArray
+        |> Seq.map (extractor)
+        |> Seq.sum
+    
+printfn "Part1: %A" (part1Result |> run)
+printfn "Part2: %A" (part2Result |> run)
--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Advent of code 2023 challenges
 
 ## Day 1
 #### Synopsis
-???
+Process a string to extract the first and last numbers - representing some coordinates.
+Part 1: Extract the first and last digits 1 to 9 in the input string
+Part 2: Extract the first and last digits 1 to 9 OR the text representaion (i.e. "one", "two", ...)
+
 #### Rating
+Part 1: Easy - simple use of Int32 Parse on the input character
+Part 2: Hard - repeatedly got the wrong answer, but finally wrote code that emulated what a human does (partly). Processing as a human is pretty easy.
+
 #### Time to complete
+5 hours
+
 #### Execution time (excludes data read)
